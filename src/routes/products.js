eb324cd97a568aa9e0f31a9d570a882d9d70bb57
Keziah Blanche T. Lastimosa <lastimosakeziahblanche@gmailcom>
2026-03-13 20:28:37 +0800

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -18,7 +18,7 @@ const upsertSchema = z.object({
   body: z.object({
     name: z.string().min(2),
     category: z.string().min(2).max(50).optional() // ISSUE-0025,
-    price: z.coerce.number() // ISSUE-0003,
+    price: z.coerce.number().min(0) // ISSUE-0003,
     stock: z.coerce.number().int().min(0),
     image_url: z.string().url().optional().nullable()
   }),
